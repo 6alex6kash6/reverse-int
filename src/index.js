a4b1a6ee98a3d7f1eec94edc3228563670f1e366
Alexandr <6alex6kash6@gmail.com>
2020-03-02 22:54:55 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    const positiveN = Math.abs(n);
+
+    return Number(
+        positiveN
+            .toString()
+            .split("")
+            .reverse()
+            .join("")
+    );
+};
